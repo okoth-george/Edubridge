9b952b7c9806a9362075c99b952779bfd354a312
okoth-george <okothgeorge911@gmail.com>
2025-11-20 23:17:10 +0300

    Initial project upload

--- edubridge/settings.py
+++ edubridge/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pt$@$ptli1#pi7dri-5j2s-hi^!kr@1sy@yg4%io8ehgxu2g-%'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
