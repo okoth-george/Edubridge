2f33e37e2da0f0ad1f8b7b06233a34549a3d73ce
okoth-george <okothgeorge911@gmail.com>
2025-12-13 06:14:29 +0300

    Deploy ready

--- edubridge/settings.py
+++ edubridge/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','edubridge.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','edubridge-fgyg.onrender.com']
                  
 
 render_host = os.getenv("RENDER_EXTERNAL_HOSTNAME")
